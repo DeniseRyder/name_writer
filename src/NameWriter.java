7fe152fed92adb50052106e6c915323690151572
DeniseRyder <dryder225@gmail.com>
2021-09-02 09:00:46 -0700

    name_writer

---
+++ src/NameWriter.java
@@ -0,0 +1,22 @@
+public class NameWriter {
+//    Write a program that displays YOUR name in a format similar to the following:
+//      DENISE
+//    M     M     A   TTTTTTT TTTTTTT
+//    M M M M    A A     T       T
+//    M  M  M   A   A    T       T
+//    M     M  AAAAAAA   T       T
+//    M     M A       A  T       T
+//
+//
+//    Upload the completed .java file to the appropriate homework link.
+
+    public static void main(String[] args){
+        System.out.println("DDD     EEEEE  N     N  IIIIIII   SSSSS   EEEEE");
+        System.out.println("D   D   E      N N   N     I     S        E    ");
+        System.out.println("D    D  EEE    N  N  N     I       SSSS   EEE  ");
+        System.out.println("D   D   E      N   N N     I           S  E    ");
+        System.out.println("DDD     EEEEE  N     N  IIIIIII   SSSSS   EEEEE");
+
+
+    }
+}
